a320431a8207789a452f737c4406f6dca80c8fc4
Dominik Rupp <dominik_rupp@gmx.de>
2016-06-30 12:02:12 +0200

    project commit

---
+++ PickerSample/PickerSample/MainView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+using Xamarin.Forms;
+
+using XLabs.Forms.Controls;
+
+namespace PickerSample {
+    public partial class MainView : ContentPage {
+        public MainView() {
+            // ExtendedPicker picker = new ExtendedPicker(); // Workaround if error: System.IO.FileNotFoundException: Could not load file or assembly 'XLabs.Forms' or one of its dependencies
+
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing() {
+            base.OnAppearing();
+
+            var viewModel = new MainViewModel();
+            viewModel.HealthyFoodCategories.Add(new FoodCategory { Id = 1, Name = "Fruits" });
+            viewModel.HealthyFoodCategories.Add(new FoodCategory { Id = 2, Name = "Vegetables" });
+            viewModel.SelectedCategory = viewModel.HealthyFoodCategories.First(); // Comment out this line, and the second picker will stay empty
+            BindingContext = viewModel;
+        }
+    }
+}
---
+++ PickerSample/PickerSample/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using PickerSample.Annotations;
+
+namespace PickerSample {
+    public class MainViewModel : INotifyPropertyChanged {
+        private FoodCategory _selectedCategory;
+        private Food _selectedFood;
+
+        public MainViewModel() {
+            HealthyFoodCategories = new ObservableCollection<FoodCategory>();
+            HealthyFoods = new ObservableCollection<Food>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<FoodCategory> HealthyFoodCategories { get; }
+
+        public ObservableCollection<Food> HealthyFoods { get; }
+
+        public FoodCategory SelectedCategory
+        {
+            get { return _selectedCategory; }
+            set
+            {
+                _selectedCategory = value;
+                if (value != null) {
+                    LoadFoods(_selectedCategory);
+                }
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedCategory)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HealthyFoods))); // desperate try to force update
+            }
+        }
+
+        public Food SelectedFood
+        {
+            get { return _selectedFood; }
+            set
+            {
+                _selectedFood = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedFood)));
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void LoadFoods(FoodCategory selectedCategory) {
+            HealthyFoods.Clear();
+            if (selectedCategory.Id == 1) {
+                HealthyFoods.Add(new Food { Id = 1, Name = "Banana" });
+                HealthyFoods.Add(new Food { Id = 2, Name = "Apple" });
+                HealthyFoods.Add(new Food { Id = 3, Name = "Pear" });
+            } else if (selectedCategory.Id == 2) {
+                HealthyFoods.Add(new Food { Id = 4, Name = "Tomato" });
+                HealthyFoods.Add(new Food { Id = 5, Name = "Carrot" });
+                HealthyFoods.Add(new Food { Id = 6, Name = "Kale" });
+            } else {
+                throw new InvalidOperationException("Unkown category");
+            }
+        }
+    }
+
+    public class FoodCategory {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString() {
+            return Id + " " + Name;
+        }
+    }
+
+    public class Food {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString() {
+            return Id + " " + Name;
+        }
+    }
+}
